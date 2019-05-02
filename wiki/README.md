0f03ed0931b60e5135e557a0a57ac0055379269d
Will Hegedus <wbhegedus@liberty.edu>
2019-05-02 15:56:04 -0400

    update nocwiki_tkts.py

---
+++ wiki/README.md
@@ -0,0 +1,19 @@
+# nocwiki_tkts
+This script was made to generate ServiceNow tickets based off a CSV list containing 2,000 wiki articles that needed to be updated.
+
+## Queries used to generate data
+```sql
+select title, c.lastmoddate from
+content c
+join spaces s on s.spaceid = c.spaceid
+where c.contenttype = 'PAGE'
+and c.prevver is null
+and s.spacename like '%OCC Team%'
+and c.title not like '%(decom%'
+order by c.lastmoddate asc
+fetch first 2000 rows only;
+```
+
+```sql
+Copy (select title, c.lastmoddate from content c join spaces s on s.spaceid = c.spaceid where c.contenttype = 'PAGE' and c.prevver is null and s.spacename like '%OCC Team%' and c.lowertitle not like '%decom%' order by c.lastmoddate asc fetch first 2000 rows only) To '/tmp/nocwiki.csv' With CSV DELIMITER ',';
+```
--- wiki/nocwiki_tkts.py
+++ wiki/nocwiki_tkts.py
@@ -1,13 +1,8 @@
 """nocwiki_tkts is a script to generate ServiceNow tickets based off a CSV"""
 
-# import importlib.util
-# spec = importlib.util.spec_from_file_location("servicenow_email", "../nagios/servicenow_email.py")
-# SN = importlib.util.module_from_spec(spec)
-# spec.loader.exec_module(SN)
-import sys
-sys.path.append('../nagios')
-
+# This is in the Nagios folder
 import servicenow_email as SN
+
 
 def chunks(l, size):
     """
@@ -15,23 +10,29 @@ def chunks(l, size):
     breaks the list into smaller chunks of the specified size
     """
     for i in range(0, len(l), size):
-        yield l[i:i+size]
+        yield l[i : i + size]
+
 
 with open("nocwiki.csv", newline="\n") as csvfile:
     LINES = list(chunks(csvfile.readlines(), 10))
     for count, group in enumerate(LINES, 1):
-        email = SN.SNEmail("liberty@service-now.com", "bwilkes5@liberty.edu", "Wiki Updates")
+        email = SN.SNEmail(
+            "liberty@service-now.com", "bwilkes5@liberty.edu", "Wiki Updates"
+        )
 
         for index, value in enumerate(group):
             group[index] = value.replace(",", " (Last updated: ").replace("\n", ")")
 
-
-        email.setBody("Wiki's to update:\n\n"
-                      + "\n".join(group)
-                      + "\n\n\nAssignment group: System Operations - OCC"
-                      + "\nRequested for: bwilkes5"
-                      + "\nItem: Add/Change/Remove Documentation"
-                      + "\nDue date: 2019-07-01 00:00:00")
+        email.setBody(
+            "Wiki's to update:\n\n"
+            + "\n".join(group)
+            + "\n\n\nAssignment group: System Operations - OCC"
+            + "\nRequested for: bwilkes5"
+            + "\nItem: Add/Change/Remove Documentation"
+            + "\nDue date: 2019-07-01 00:00:00"
+        )
         # print(email.msg)
-        email.send()
+
+        # UNCOMMENT THIS TO SEND THE EMAILS
+        # email.send()
         print("Sent email {0} of 200".format(count))
