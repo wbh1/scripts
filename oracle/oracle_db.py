9d10474919d83d15ab17bcce5a619433e24f2d4f
Ian Pringle <ipringle2@liberty.edu>
2019-08-03 11:43:34 -0400

    Linting

--- oracle/oracle_db.py
+++ oracle/oracle_db.py
@@ -1,7 +1,7 @@
 import cx_Oracle
 import os
 
-# pylint: disable=too-few-public-methods
+
 class OracleDB:
     """
     Generic Class for interacting with Oracle DBs
@@ -15,46 +15,46 @@ class OracleDB:
                 print(
                     "ERROR:",
                     "============",
-                    "Specify a user attribute in a creds.py file in the same directory as this script",
+                    ("Specify a user attribute in a creds.py "
+                     "file in the same directory as this script"),
                     sep="\n",
                 )
                 exit(1)
-    
+
             try:
                 p_ora = creds.password
             except AttributeError:
                 print(
                     "ERROR:",
                     "============",
-                    "Specify a user attribute in a creds.py file in the same directory as this script",
+                    ("Specify a user attribute in a creds.py "
+                     "file in the same directory as this script"),
                     sep="\n",
                 )
                 exit(1)
         except ModuleNotFoundError:
             try:
                 u_ora, p_ora = self.get_creds('whprd')
-            except:
-                print("ERROR:\n============\nYou need to create a creds.py file or setup use the OS credential locker")
+            except:  # noqa: E722
+                print("ERROR:\n============\nYou need to create a creds.py "
+                      "file or setup use the OS credential locker")
                 exit(1)
 
         self.con = cx_Oracle.connect(u_ora, p_ora, database)
         self.cur = self.con.cursor()
+        self.errors = []
 
     def close(self):
         """
         Close cursor, commit transaction, close connection.
         """
-        ###################################
-        ######## Print any errors #########
-        ###################################
+        # Print any errors
         if self.errors:
             print("\nErrors:\n=================")
             for err in self.errors:
                 print(err)
 
-        ###################################
-        #### Commit and close DB stuff ####
-        ###################################
+        # Commit and close DB stuff
         self.cur.close()
         self.con.commit()
         self.con.close()
@@ -75,4 +75,3 @@ class OracleDB:
             ring.set_password(service, user, password)
             password = ring.get_password(service, user)
         return user, password
-    
